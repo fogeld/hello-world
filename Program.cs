24b4a0c51507f241401a0c15f284e8f20e858767
fogeld <fogel.davide@gmail.com>
2021-04-26 19:16:45 +0300

    my first program

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your name?");
+        String name = Console.ReadLine();
+        Console.WriteLine("Hello " + name);
+    }
+}
